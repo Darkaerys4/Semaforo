396d35a53de6f3e8ce8eb077f944ad4d181ba98b
Darkaerys4 <arthurgato2001@gmail.com>
2024-02-05 13:06:07 +0100

    Finalizado-el-cambio-de-los-semaforos-de-peatones

--- js/semaforoPeatones.js
+++ js/semaforoPeatones.js
@@ -1,19 +1,38 @@
 $(document).ready(function () {
-    $(".btnIz-Peaton").click(function (e) {
-        e.preventDefault();
-        setTimeout(() => {
-            controlPeatones();
-        }, 3000);
+    var pulsarBoton = false;
+    $(".btnIz-Peaton").click(function () {
+        if (!pulsarBoton) {
+            pulsarBoton = true;
+            var contador1 = 3;
+            $(".contPeatones").text(contador1);
+            var intervalo1 = setInterval(() => {
+                contador1--;
+                $(".contPeatones").text(contador1);
+                if (contador1 == 0) {
+                    clearInterval(intervalo1);
+                    controlPeatones("verde", "rojo")
+                }
+            }, 1000);
+        }
     });
 
-    function cambioPeatonesIzquierda() {
-
-        $(".CambioRojo").attr("src", "../recursos/img/semafororojo.png");
-        $(".CambioVerde").attr("src", "../recursos/img/semaforoverde.png");
-    }
-
-    function controlPeatones() {
-        $(".img-rojo").attr("src", "../recursos/img/Peatonesverde.png");
-        $(".img-verde").attr("src", "../recursos/img/PeatonesrojoVertical.png");
+    function controlPeatones(color1, color2) {
+        $(".img-rojo").attr("src", "../recursos/img/Peatones" + color1 + ".png");
+        $(".img-verde").attr("src", "../recursos/img/Peatones" + color2 + "Vertical.png");
+        var segundos = 3;
+        $(".contPeatones").text(segundos);
+        intervalo2 = setInterval(function () {
+            segundos--;
+            $(".contPeatones").text(segundos);
+            if (segundos == 0) {
+                clearInterval(intervalo2);
+                if (color1 == "verde" && color2 == "rojo") {
+                    pulsarBoton = false;
+                    controlPeatones("rojo", "verde")
+                    clearInterval(intervalo2);
+                    $(".contPeatones").text(" ");
+                }
+            }
+        }, 1000);
     }
 });
