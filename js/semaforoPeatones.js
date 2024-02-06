fd300a18b31ec070c38be1e9031c22238e561d78
Romlan03 <markelromero03@gmail.com>
2024-02-06 10:49:15 +0100

    mapa-esperemos-que-definitivo

--- js/semaforoPeatones.js
+++ js/semaforoPeatones.js
@@ -1,10 +1,26 @@
 $(document).ready(function () {
 
-    let peatonesDerecha = $('.peatones-derecha');
-    const PEATON_VERDE = '../recursos/img/Peatonesverde.png';
+    let peatonesDerechaH = $('.peatones-derecha');
+    let peatonesDerechaV = $('.peatones-derecha-vertical');
+    let tmp = 0;
+    const PEATON_VERDE_HORIZONTAL = '../recursos/img/Peatonesverde.png';
+    const PEATON_VERDE_VERTICAL = '../recursos/img/PeatonesverdeVertical.png';
+    const PEATON_ROJO_HORIZONTAL = '../recursos/img/Peatonesrojo.png';
+    const PEATON_ROJO_VERTICAL = '../recursos/img/PeatonesrojoVertical.png';
 
-    $(peatonesDerecha).click(function (e) {
-        setInterval(peatonesDerecha,3000);
-        peatonesDerecha.attr('src', PEATON_VERDE);
+    $(peatonesDerechaH).click(function (e) {
+        setInterval(() => {
+            peatonesDerechaH.attr('src', PEATON_VERDE_HORIZONTAL);
+            peatonesDerechaV.attr('src', PEATON_ROJO_VERTICAL);
+            tmp++;
+            console.log(tmp);
+            if (tmp == 5) {
+                peatonesDerechaH.attr('src', PEATON_ROJO_HORIZONTAL);
+                peatonesDerechaV.attr('src', PEATON_VERDE_VERTICAL);
+            }
+            
+        }, 1000);
         });
+
+        
 });
