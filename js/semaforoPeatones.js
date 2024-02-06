2d9a92430011a1af30192c29ac72f8aac713b72a
Darkaerys4 <arthurgato2001@gmail.com>
2024-02-06 12:42:51 +0100

    Ultimo cambio

--- js/semaforoPeatones.js
+++ js/semaforoPeatones.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
         if (!pulsarBoton) {
             pulsarBoton = true;
             var contador1 = 3;
-            $(".contador-verde-horizontal").text(contador1);
+            $(".contPeatones").text(contador1);
             var intervalo1 = setInterval(() => {
                 contador1--;
-                $(".contador-verde-horizontal").text(contador1);
+                $(".contPeatones").text(contador1);
                 if (contador1 == 0) {
                     clearInterval(intervalo1);
                     controlPeatones("verde", "rojo")
@@ -20,17 +20,17 @@ $(document).ready(function () {
         $(".img-rojo").attr("src", "../recursos/img/Peatones" + color1 + ".png");
         $(".img-verde").attr("src", "../recursos/img/Peatones" + color2 + "Vertical.png");
         var segundos = 3;
-        $(".contador-verde-horizontal").text(segundos);
+        $(".contPeatones").text(segundos);
         intervalo2 = setInterval(function () {
             segundos--;
-            $(".contador-verde-horizontal").text(segundos);
+            $(".contPeatones").text(segundos);
             if (segundos == 0) {
                 clearInterval(intervalo2);
                 if (color1 == "verde" && color2 == "rojo") {
                     pulsarBoton = false;
                     controlPeatones("rojo", "verde")
                     clearInterval(intervalo2);
-                    $(".contador-verde-horizontal").text(" ");
+                    $(".contPeatones").text(" ");
                 }
             }
         }, 1000);
