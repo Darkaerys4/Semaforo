f39af3f5ac112305bbab92b874f20e64961ecb49
Darkaerys4 <arthurgato2001@gmail.com>
2024-02-07 13:33:08 +0100

    Cambio-semaforo-izquierda

--- js/semaforoPeatones.js
+++ js/semaforoPeatones.js
@@ -1,38 +1,108 @@
 $(document).ready(function () {
-    var pulsarBoton = false;
-    $(".btnIz-Peaton").click(function () {
-        if (!pulsarBoton) {
-            pulsarBoton = true;
-            var contador1 = 3;
-            $(".contPeatones").text(contador1);
-            var intervalo1 = setInterval(() => {
-                contador1--;
-                $(".contPeatones").text(contador1);
-                if (contador1 == 0) {
-                    clearInterval(intervalo1);
-                    controlPeatones("verde", "rojo")
+    $(".btnIz-Peaton-Ar").each(function () {
+        var pulsarBoton = false;
+        $(this).click(function () {
+            if (!pulsarBoton) {
+                pulsarBoton = true;
+                var contador1 = 3;
+                $(".contPeatones-Ar-1").text(contador1);
+                var intervalo1 = setInterval(() => {
+                    contador1--;
+                    $(".contPeatones-Ar-1").text(contador1);
+                    if (contador1 == 1) {
+                        $(".sema-Co-Ar").attr("src", "../recursos/img/semafororojo.png");
+                    }
+                    else if (contador1 == 0) {
+                        clearInterval(intervalo1);
+                        controlPeatonesAr1("verde", "rojo")
+                    }
+                }, 1000);
+            }
+        });
+        function controlPeatonesAr1(colorP, colorS) {
+            $(".sema-Co-Ar").attr("src", "../recursos/img/semaforo"+colorS+".png");
+            $(".img-rojo-Vr-1").attr("src", "../recursos/img/Peatones" + colorP + "Vertical.png");
+            var segundos = 3;
+            $(".contPeatones-Ar-1").text(segundos);
+            intervalo2 = setInterval(function () {
+                segundos--;
+                $(".contPeatones-Ar-1").text(segundos);
+                if (segundos == 0) {
+                    clearInterval(intervalo2);
+                    cambioARojoVerAr();
                 }
             }, 1000);
+            //Parte que devuelve la imagen  
+            function cambioARojoVerAr() {
+                if (colorP == "verde") {
+                    pulsarBoton = false;
+                    controlPeatonesAr1("rojo", "rojo")
+                    clearInterval(intervalo2);
+                    $(".contPeatones-Ar-1").text(" ");
+                    if (colorS == "rojo") {
+                        setTimeout(function () {
+                            controlPeatonesAr1("rojo", "ambar")
+                            clearInterval(intervalo2);
+                            $(".contPeatones-Ar-1").text(" ");
+                        }, 2000);
+                    }
+
+                }
+            }
         }
     });
 
-    function controlPeatones(color1, color2) {
-        $(".img-rojo").attr("src", "../recursos/img/Peatones" + color1 + ".png");
-        $(".img-verde").attr("src", "../recursos/img/Peatones" + color2 + "Vertical.png");
-        var segundos = 3;
-        $(".contPeatones").text(segundos);
-        intervalo2 = setInterval(function () {
-            segundos--;
-            $(".contPeatones").text(segundos);
-            if (segundos == 0) {
-                clearInterval(intervalo2);
-                if (color1 == "verde" && color2 == "rojo") {
-                    pulsarBoton = false;
-                    controlPeatones("rojo", "verde")
+    $(".btnIz-Peaton-Ab").each(function () {
+        var pulsarBoton = false;
+        $(this).click(function () {
+            if (!pulsarBoton) {
+                pulsarBoton = true;
+                var contador1 = 3;
+                $(".contPeatones-Ab-1").text(contador1);
+                var intervalo1 = setInterval(() => {
+                    contador1--;
+                    $(".contPeatones-Ab-1").text(contador1);
+                    if (contador1 == 1) {
+                        $(".sema-Co-Ab").attr("src", "../recursos/img/semafororojo.png");
+                    }
+                    else if (contador1 == 0) {
+                        clearInterval(intervalo1);
+                        controlPeatonesAr2("verde", "rojo")
+                    }
+                }, 1000);
+            }
+        });
+        function controlPeatonesAr2(colorP, colorS) {
+            $(".sema-Co-Ab").attr("src", "../recursos/img/semaforo"+colorS+".png");
+            $(".img-rojo-Vr-2").attr("src", "../recursos/img/Peatones" + colorP + "Vertical.png");
+            var segundos = 3;
+            $(".contPeatones-Ab-1").text(segundos);
+            intervalo2 = setInterval(function () {
+                segundos--;
+                $(".contPeatones-Ab-1").text(segundos);
+                if (segundos == 0) {
                     clearInterval(intervalo2);
-                    $(".contPeatones").text(" ");
+                    cambioARojoVerAr2();
+                }
+            }, 1000);
+            //Parte que devuelve la imagen  
+            function cambioARojoVerAr2() {
+                if (colorP == "verde") {
+                    pulsarBoton = false;
+                    controlPeatonesAr2("rojo", "rojo")
+                    clearInterval(intervalo2);
+                    $(".contPeatones-Ab-1").text(" ");
+                    if (colorS == "rojo") {
+                        setTimeout(function () {
+                            controlPeatonesAr2("rojo", "ambar")
+                            clearInterval(intervalo2);
+                            $(".contPeatones-Ab-1").text(" ");
+                        }, 2000);
+                    }
+
                 }
             }
-        }, 1000);
-    }
+        }
+    });
+
 });
