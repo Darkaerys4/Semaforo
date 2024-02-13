7f8c36b4812d94e591045e593c0c5817aaa95813
junior <juniorquispe1314@gmail.com>
2024-02-13 12:19:05 +0100

    sema-zona1-arriba-hechos

--- js/pruebas.js
+++ js/pruebas.js
@@ -1,18 +1,11 @@
 $(document).ready(function(){
 
-    //semaforos peatones izquierda derecha
-    let btnIz_peatonDer = $('.btnIz-Peaton-Iz-2');
-    let contIz_der = $('#cont-p-iz-der');
-
-    //semaforos coches PRICIPALES
-    let carretera1 = $('#carretera1');
-    let carretera11 = $('#carretera11');
-
-    //semaforos coches CARRETERA SECUNDARIA IZQ
+    //semaforos carretera izquierda arriba
+    let carretera5 = $('#carretera5');
     let carretera8 = $('#carretera8');
-    let carretera4 = $('#carretera4');
 
-    let intervalId_der;
+    let contIz_ar = $('#contPeatones-Ar-1');
+
     //tiempos de espera
     const ESPERA_ROJO_P = 5;
     const ESPERA_VERDE_P = 8;
@@ -20,15 +13,11 @@ $(document).ready(function(){
     const EMPEZAR_AMBAR_C = 2;
     const AMBOS_ROJO = 1;
 
-    //variables que controlan el contador
-    let tiempoGeneral_der = 0;
-    let labelPeaton_der = ESPERA_ROJO_P;
-    let presionado_der = false;
+    let intervalId_ar;
 
-
-    //semaforos peatonales
-    const ROJO_HORIZ_P = '../recursos/img/Peatonesrojo.png';
-    const VERDE_HORIZ_P = '../recursos/img/Peatonesverde.png';
+    let presionado_ar = false;
+    let tiempoGeneral_ar = 0;
+    let labelPeaton_ar = ESPERA_ROJO_P;
 
     const ROJO_VERT_P = '../recursos/img/PeatonesrojoVertical.png';
     const VERDE_VERT_P = '../recursos/img/PeatonesverdeVertical.png';
@@ -38,57 +27,54 @@ $(document).ready(function(){
     const AMBAR_C = '../recursos/img/semaforoambar.png';
     const VERDE_C = '../recursos/img/semaforoverde.png';
 
+    //semaforos peatones
+    let btnIz_peatonIz = $('.btnIz-Peaton-Ar');
 
-    btnIz_peatonDer.on( 'click', function() {
-        console.log('BOTON PRESIONADO IZQUIERDA DERECHA')
-
-        if(presionado_der){
+    btnIz_peatonIz.on( 'click', function() {
+        if(presionado_ar){
             console.log('NO PUDES PRESIONARLO OTRA VEZ')
             return;
         }
-        contIz_der.text(ESPERA_ROJO_P);
-        presionado_der = true;
-        contIz_der.css({ opacity: 1 });
-        intervalId_der = setInterval(iniciarPeatonesDerecha,1000);
+
+        contIz_ar.text(ESPERA_ROJO_P);
+        presionado_ar = true;
+        contIz_ar.css({ opacity: 1 });
+        intervalId_ar = setInterval(iniciarPeatonesIzquierda,1000);
 
     });
 
+    function iniciarPeatonesIzquierda(){
+        tiempoGeneral_ar++;
+        console.log(tiempoGeneral_ar);
+        labelPeaton_ar--;
+        contIz_ar.text(labelPeaton_ar);
 
-    function iniciarPeatonesDerecha(){
-        tiempoGeneral_der++;
-        console.log(tiempoGeneral_der);
-        labelPeaton_der--;
-        contIz_der.text(labelPeaton_der);
+        if(tiempoGeneral_ar == ESPERA_ROJO_P - EMPEZAR_AMBAR_C + AMBOS_ROJO){ // 5-2+1 = 4
+            cambiarSemaforoC([carretera8, carretera5], ROJO_C);
 
-        if(tiempoGeneral_der == ESPERA_ROJO_P - EMPEZAR_AMBAR_C){ // 5-2 = 3
-            cambiarSemaforoC([carretera1, carretera11, carretera8, carretera4], AMBAR_C);
-
-        }else if(tiempoGeneral_der == ESPERA_ROJO_P - EMPEZAR_AMBAR_C + AMBOS_ROJO){ // 5-2+1 = 4 
-            cambiarSemaforoC([carretera1, carretera11, carretera8, carretera4], ROJO_C);
-
-        }else if(tiempoGeneral_der == ESPERA_ROJO_P){ //5
-            contIz_der.text(ESPERA_VERDE_P);
-            labelPeaton_der = ESPERA_VERDE_P;
+        }else if(tiempoGeneral_ar == ESPERA_ROJO_P){ //5
+            contIz_ar.text(ESPERA_VERDE_P);
+            labelPeaton_ar = ESPERA_VERDE_P;
             console.log('pasar a verde');
-            $(btnIz_peatonDer).children('img').attr('src', VERDE_HORIZ_P);
+            $(btnIz_peatonIz).children('img').attr('src', VERDE_VERT_P);
 
-        }else if(tiempoGeneral_der == ESPERA_ROJO_P + ESPERA_VERDE_P - AMBOS_ROJO){
-            $(btnIz_peatonDer).children('img').attr('src', ROJO_HORIZ_P);
-        }else if(tiempoGeneral_der == ESPERA_ROJO_P + ESPERA_VERDE_P){
-            console.log('pasa a rojo');
-            $(btnIz_peatonDer).children('img').attr('src', ROJO_HORIZ_P);
-            clearInterval(intervalId_der);
-            labelPeaton_der = ESPERA_ROJO_P;
-            contIz_der.css({ opacity: 0 });
-            intervalId_der = null;
-            tiempoGeneral_der = 0;
-            presionado_der = false;
-            cambiarSemaforoC([carretera1, carretera11], VERDE_C);
-            cambiarSemaforoC([ carretera8, carretera4], AMBAR_C);
-            
+        }else if(tiempoGeneral_ar == ESPERA_VERDE_P + ESPERA_ROJO_P){ //13
+            $(btnIz_peatonIz).children('img').attr('src', ROJO_VERT_P);
+            contIz_ar.css({ opacity: 0 });
         }
-
+        else if(tiempoGeneral_ar == ESPERA_VERDE_P + ESPERA_ROJO_P + AMBOS_ROJO){ //14
+            cambiarSemaforoC([carretera8, carretera5], AMBAR_C);
+            clearInterval(intervalId_ar);
+            labelPeaton_ar = ESPERA_ROJO_P;
+            contIz_ar.css({ opacity: 0 });
+            intervalId_ar = null;
+            tiempoGeneral_ar = 0;
+            presionado_ar = false;
+            console.log('ULTIMO');
+        }
     }
+
+
 
     function cambiarSemaforoC(list, fotoCambiar){
         for(let i = 0 ; i < list.length; i++){
